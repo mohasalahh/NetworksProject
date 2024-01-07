0bf28d1183a0e8cc8ce7c3a711022858e056dd96
Mohamed Salah <me@mohasalah.com>
2024-01-07 21:53:38 +0200

    add encryption testing

--- test.py
+++ test.py
@@ -3,4 +3,5 @@ from Utils import AESEnryptionUtils
 encrypted = AESEnryptionUtils.AESEncryption().encrypt("allo")
 decrypted = AESEnryptionUtils.AESEncryption().decrypt(encrypted)
 
+print(encrypted)
 print(decrypted)
