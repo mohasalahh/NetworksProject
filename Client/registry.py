103847ec9c6e71529ac52ad97ef0533f20af4fc8
Mohamed Salah <me@mohasalah.com>
2024-01-07 22:33:41 +0200

    add error handling

--- Client/registry.py
+++ Client/registry.py
@@ -1,8 +1,15 @@
 from Client.RegistryThread import RegistryThread
 
-try:
-    registryThread = RegistryThread()
-    registryThread.start()
-    registryThread.join()
-except Exception as error:
-    print("Error: {0}".format(error))
+
+def start_thread():
+    try:
+        registryThread = RegistryThread()
+        registryThread.start()
+        registryThread.join()
+    except Exception as error:
+        print("Error: {0}".format(error))
+        print("Restarting Registry...")
+        start_thread()
+
+
+start_thread()
--- Peer/peer.py
+++ Peer/peer.py
@@ -1,8 +1,15 @@
 from Peer.PeerThread import PeerThread
 
-try:
-    main = PeerThread()
-    main.start()
-    main.join()
-except Exception as error:
-    print("Error: {0}".format(error))
+
+def start_thread():
+    try:
+        main = PeerThread()
+        main.start()
+        main.join()
+    except Exception as error:
+        print("Error: {0}".format(error))
+        print("Restarting Peer...")
+        start_thread()
+
+
+start_thread()
